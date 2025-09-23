d097f8349951e6b2b1a2ca4b6de6a3b3f6514d76
Aquib younis <aquibyounis1@gmail.com>
2025-09-24 00:49:46 +0530

    Some changes 3

--- compass/src/Pages/Home/Home.jsx
+++ compass/src/Pages/Home/Home.jsx
@@ -7,7 +7,11 @@ const Home = () => {
         <h1 className="h1">Hey VITIAN! How can I help you?</h1>
         <div className="chat">
             <textarea name="" id="" className="textarea" placeholder="Type your Query..."></textarea>
+            
+            <div className="btnss">
+              <button className="btn3"><i class="fa-solid fa-microphone-lines"></i></button>
             <button className="btn"><i class="fa-solid fa-arrow-up"></i></button>
+            </div>
         </div>
     </div>
   )

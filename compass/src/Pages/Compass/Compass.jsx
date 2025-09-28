b5b08b0638f5ed409216ac8599e9fb1fd68365f6
Aquib younis <aquibyounis1@gmail.com>
2025-09-28 23:00:41 +0530

    Working

--- compass/src/Pages/Compass/Compass.jsx
+++ compass/src/Pages/Compass/Compass.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import "./Compass.css"
 import Mainbar from '../../Components/mainbar/Mainbar'
 import Buildings from '../../Components/Buildings/Buildings'
+import {useNavigate} from "react-router-dom";
 
 const Compass = () => {
+    const navigate = useNavigate(); // <-- hook
+  
   return (
     <div className='compass'>
         <Mainbar/>
         <Buildings/>
         <div className="btnn">
-          <button className='btn2'>Visit AI <i class="fa-solid fa-up-right-from-square"></i></button>
+          <button className='btn2' onClick={() => navigate("/")}>Visit AI <i class="fa-solid fa-up-right-from-square"></i></button>
         </div>
     </div>
   )
